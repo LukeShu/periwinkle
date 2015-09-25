08023ceeda3c89ef0f9b6d9d3c991986c569d6bb
Luke Shumaker <shumakl@purdue.edu>
2015-09-25 12:09:40 -0400

    add listener process

---
+++ src/listener/listener_process.go
@@ -0,0 +1,19 @@
+// Copyright 2015 Luke Shumaker
+
+package main
+
+import (
+	"listeners/twilio"
+	"listeners/maildir"
+	"listeners/web"
+	"sync"
+)
+
+func main() {
+	var wg sync.WaitGroup;
+	wg.Add(3)
+	go func() { twilio.Main(); wg.Done(); }()
+	go func() { maildir.Main(); wg.Done(); }()
+	go func() { web.Main(); wg.Done(); }()
+	wg.Wait()
+}
