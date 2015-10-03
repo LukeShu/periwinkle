b75fee855caa426b4b9bdd7f638b827d5f137725
Davis Webb <davislwebb@ymail.com>
2015-10-03 17:14:45 -0400

    DavisLWebb
    fleshed out medium.go ORM

--- src/orm/medium.go
+++ src/orm/medium.go
@@ -6,3 +6,18 @@ import _ "github.com/go-sql-driver/mysql"
 type Medium struct{
 	id int
 }
+
+func getMediumById(id int)(*Medium, error){
+	var med Medium
+	err := con.QueryRow("select * from group_addresses where id=?",id).Scan(&med)
+	switch {
+		case err == sql.ErrNoRows:
+			// group does not exist
+			return nil, nil
+		case err != nil:
+			// error talking to the DB
+			return nil, err
+		default:
+			return &med, nil
+	}
+}
