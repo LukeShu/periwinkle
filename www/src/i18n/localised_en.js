556378813af6b5c92d52b41591ff9ad268736311
Richard L. Wisniewski <r.wisniewski1101@gmail.com>
2015-10-03 01:52:34 -0400

    www - localisation is updated for error messages
    italian local is iffy until I meet with my professor on moday to check
    them.

--- www/src/i18n/localised_en.js
+++ www/src/i18n/localised_en.js
@@ -4,11 +4,14 @@ localised.en = {
 	EMAIL:			"Email",
 	PASSWORD:		"Password",
 	LOGIN:			"Login",
+	REQUIRED_FIELD:	"This field is required.",
 	SIGNUP: {
-		SIGNUP:				"Sign Up",
-		NOT_A_USER:			"Not a user yet?",
-		ALREADY_USER:		"Aready a user?",
-		CONFIRM_EMAIL:		"Confirm Email",
-		CONFIRM_PASSWORD:	"Confirm password"
+		SIGNUP:						"Sign Up",
+		NOT_A_USER:					"Not a user yet?",
+		ALREADY_USER:				"Aready a user?",
+		CONFIRM_EMAIL:				"Confirm Email",
+		CONFIRM_EMAIL_NO_MATCH:		"Your email addresses do not match.",
+		CONFIRM_PASSWORD:			"Confirm password",
+		CONFIRM_PASSWORD_NO_MATCH:	"Your passwords do not match"
 	}
 };
