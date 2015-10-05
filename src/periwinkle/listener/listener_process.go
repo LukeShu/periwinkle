e559addda373f91f44ea72a4b1df485565ebbefc
Luke Shumaker <shumakl@purdue.edu>
2015-10-05 16:49:58 -0400

    gofmt

--- src/periwinkle/listener/listener_process.go
+++ src/periwinkle/listener/listener_process.go
@@ -4,16 +4,18 @@ package main
 
 import (
 	//"periwinkle/listeners/twilio"
+	"fmt"
 	"periwinkle/listeners/maildir"
 	"periwinkle/listeners/web"
 	"sync"
 )
 
 func main() {
+	fmt.Println("listener starting")
 	var wg sync.WaitGroup
 	wg.Add(3)
-	//go func() { twilio.Main(); wg.Done(); }()
+	//go func() { twilio.Main(); wg.Done() }()
 	go func() { maildir.Main(); wg.Done() }()
-	go func() { web.Main(); wg.Done(); }()
+	go func() { web.Main(); wg.Done() }()
 	wg.Wait()
 }
