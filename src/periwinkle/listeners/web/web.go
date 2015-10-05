f05dc66055badb5508f60ff8ca5a3aa41f909277
Luke Shumaker <shumakl@purdue.edu>
2015-10-05 15:02:18 -0400

    implement the web listener

---
+++ src/periwinkle/listeners/web/web.go
@@ -0,0 +1,26 @@
+// Copyright 2015 Luke Shumaker
+
+package web
+
+import (
+	"periwinkle/cfg"
+	"periwinkle/store"
+	"net/http"
+	he "httpentity"
+	"time"
+)
+
+func Main() error {
+	mux := http.NewServeMux()
+	mux.Handle("/v1/"   , he.NetHttpHandler("/v1/", store.DirRoot))
+	mux.Handle("/webui/", http.StripPrefix("/webui/", http.FileServer(cfg.WebUiDir)))
+	server := &http.Server{
+		Addr:           cfg.WebAddr,
+		Handler:        mux,
+		ReadTimeout:    10 * time.Second,
+		WriteTimeout:   10 * time.Second,
+		MaxHeaderBytes: 1 << 20,
+	}
+	server.ListenAndServe()
+	panic("not reached")
+}
