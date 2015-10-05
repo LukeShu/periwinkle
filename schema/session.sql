926a365a16f0e739a307534cb2c89974fca0ea75
Guntas Grewal <guntasgrewal@gmail.com>
2015-10-05 18:57:57 -0400

    changed session.sql to have user_id

--- schema/session.sql
+++ schema/session.sql
@@ -1,4 +1,3 @@
 create table `session` (`id` INT(11) UNSIGNED NOT NULL AUTO_INCREMENT, 
-	`fullname` VARCHAR(20) NOT NULL,
-	`passwordHash` VARCHAR(40) NOT NULL,
+	`user_id` VARCHAR(40) NOT NULL,
 	PRIMARY KEY (`id`)) ENGINE=MyISAM;
