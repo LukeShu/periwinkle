b12e752a6705da9e6d75eed6a23501d95161cf56
Richard L. Wisniewski <r.wisniewski1101@gmail.com>
2015-10-09 00:43:37 -0400

    www - viewing a user works with lorum ipsum

--- www/src/user/UserController.js
+++ www/src/user/UserController.js
@@ -3,20 +3,18 @@
 
 	angular
 		.module('user')
-		.controller('UserController', ['$cookies', '$http', '$scope', '$interval', UserController]); 
+		.controller('UserController', ['$cookies', '$http', '$scope', '$interval', UserController]);
 
 	function UserController($cookies, $http, $scope, $interval) {
 		//gives us an anchor to the outer object from within sub objects or functions
 		var self = this;
 		//clears the toolbar and such so we can set it up for this view
 		$scope.resetHeader();
-		$scope.toolbar.title = "MOOOO";
+		$scope.toolbar.title = "User";
 		//set up public fields
-		self.username = '';
-		self.password = '';
-		self.email = '';
-		
-		alert();
+		self.username = 'Richard Wisniewski';
+		self.email = 'rwisniew@purdue.edu';
+		self.sessionID = "0x1234567890";
 		
 		self.reload = function() {
 			
