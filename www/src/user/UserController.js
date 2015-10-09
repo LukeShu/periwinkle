529483590befc5bd61c3dd9ded56356f990f2d2c
Richard L. Wisniewski <r.wisniewski1101@gmail.com>
2015-10-08 20:23:09 -0400

    www - testing $scope issues

--- www/src/user/UserController.js
+++ www/src/user/UserController.js
@@ -10,6 +10,7 @@
 		var self = this;
 		//clears the toolbar and such so we can set it up for this view
 		$scope.resetHeader();
+		$scope.tootbar.title = "";
 		//set up public fields
 		self.username = '';
 		self.password = '';
