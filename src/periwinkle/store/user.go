51cc1714ee5937ec38135a96bf245b146ea5e7bc
Davis Webb <davislwebb@ymail.com>
2015-10-21 18:49:45 -0400

    DavisLWebb & MarkPundmann
    Possibly fixed user login/save in terms of the server

--- src/periwinkle/store/user.go
+++ src/periwinkle/store/user.go
@@ -46,7 +46,7 @@ func (o UserAddress) schema(db *gorm.DB) {
 
 func GetUserById(db *gorm.DB, id string) *User {
 	var o User
-	if result := db.First(&o, id); result.Error != nil {
+	if result := db.Where("id=?", strings.ToLower(id)).First(&o); result.Error != nil {
 		if result.RecordNotFound() {
 			return nil
 		}
@@ -161,6 +161,7 @@ func newDirUsers() t_dirUsers {
 			if user == nil {
 				return req.StatusConflict(heutil.NetString("either that username or password is already taken"))
 			} else {
+				user.Save(db)
 				return req.StatusCreated(r, username)
 			}
 		},
