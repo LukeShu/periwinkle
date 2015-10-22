c3adc088661f2f1750a72a0cbbed2dc55724b42a
Luke Shumaker <shumakl@purdue.edu>
2015-10-21 21:01:10 -0400

    gofmt

--- src/httpentity/router.go
+++ src/httpentity/router.go
@@ -31,7 +31,7 @@ func normalizeURL(u1 *url.URL) (u *url.URL, mimetype string) {
 
 // assumes that the url has already been passed to normalizeURL()
 func (r *Router) route(req Request, u *url.URL) (res Response) {
-	if (r.LogRequest) {
+	if r.LogRequest {
 		fmt.Fprintf(os.Stderr, "%s %q %#v\n", req.Method, u.String(), req)
 	}
 	// do the routing
