3a23e34c066529507efda6a231d0cebedf55a985
Richard L. Wisniewski <r.wisniewski1101@gmail.com>
2015-10-22 00:46:42 -0400

    www - cleaned things up and added a logout to UserService

--- www/src/UserService.js
+++ www/src/UserService.js
@@ -7,9 +7,42 @@
 		.service('UserService', ['$scope', '$http', '$translate', UserService]);
 		
 	function UserService ($scope, $http, $translate) {
-		self = this;
-		self.username = '';
-		self.session_id = '';
-		self.language = '';
+		var self = this;
+		
+		self.reset = function() {
+			self.username = '';
+			self.session_id = '';
+			self.language = '';
+		};
+		self.reset();
+		
+		self.logout = function () {
+			if(session_id && session_id != "") {
+				$http({
+					method: 'DELETE',
+					url: '/v1/session',
+					headers: {
+						'Content-Type': 'application/json'
+					},
+					data: {
+						session_id: self.session_id
+					}
+				}).then(
+					function success(data, status, headers, config) {
+						//do work with response
+						debugger;
+						self.reset();
+						$location.path('/login').replace();
+					},
+					function fail(data, status, headers, config) {
+						//do work with response
+						//show error to user
+						debugger;
+						$scope.loading.is = false;
+						//show alert
+					}
+				);
+			}
+		};
 	}
 })();
