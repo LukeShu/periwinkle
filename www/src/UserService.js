0217b0ea2cdf57cbcd5e58a5477c1fb3843f3b73
Richard L. Wisniewski <r.wisniewski1101@gmail.com>
2015-10-26 00:31:46 -0400

    fixed an issue with UserService

--- www/src/UserService.js
+++ www/src/UserService.js
@@ -4,9 +4,9 @@
 
 	angular
 		.module('periwinkle.UserService', [])
-		.service('UserService', ['$scope', '$http', '$translate', UserService]);
+		.service('UserService', [UserService]);
 		
-	function UserService ($scope, $http, $translate) {
+	function UserService () {
 		var self = this;
 		
 		self.reset = function() {
@@ -15,34 +15,5 @@
 			self.language = '';
 		};
 		self.reset();
-		
-		$scope.logout = self.logout = function () {
-			if(session_id && session_id != "") {
-				$http({
-					method: 'DELETE',
-					url: '/v1/session',
-					headers: {
-						'Content-Type': 'application/json'
-					},
-					data: {
-						session_id: self.session_id
-					}
-				}).then(
-					function success(data, status, headers, config) {
-						//do work with response
-						debugger;
-						self.reset();
-						$location.path('/login').replace();
-					},
-					function fail(data, status, headers, config) {
-						//do work with response
-						//show error to user
-						debugger;
-						$scope.loading.is = false;
-						//show alert
-					}
-				);
-			}
-		};
 	}
 })();
--- www/src/controller.js
+++ www/src/controller.js
@@ -4,9 +4,9 @@
 
 	angular
 		.module('periwinkle')
-		.controller('PeriwinkleController', ['$scope', PeriwinkleController]);
+		.controller('PeriwinkleController', ['$scope', 'UserService', PeriwinkleController]);
 		
-	function PeriwinkleController ($scope) {
+	function PeriwinkleController ($scope, userService) {
 		var resetHeader = function() {
 			$scope.sidenav = {
 				exists: false,
@@ -27,5 +27,35 @@
 			};
 		}
 		$scope.resetHeader = resetHeader;
+		
+		$scope.logout = function () {
+			$scope.loading.is = true;
+			if(userService.session_id && userService.session_id != "") {
+				$http({
+					method: 'DELETE',
+					url: '/v1/session',
+					headers: {
+						'Content-Type': 'application/json'
+					},
+					data: {
+						session_id: userService.session_id
+					}
+				}).then(
+					function success(response) {
+						//do work with response
+						debugger;
+						self.reset();
+						$location.path('/login').replace();
+					},
+					function fail(response) {
+						//do work with response
+						//show error to user
+						debugger;
+						$scope.loading.is = false;
+						//show alert
+					}
+				);
+			}
+		};
 	}
 })();
