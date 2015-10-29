8803e0121179d9c5c53230e61b9efafb16e5bdc7
Luke Shumaker <shumakl@purdue.edu>
2015-10-28 22:55:36 -0400

    middleware_database: detect SQLite constraint failures, and HTTP 409 them

--- src/periwinkle/listeners/web/middleware_database.go
+++ src/periwinkle/listeners/web/middleware_database.go
@@ -3,8 +3,11 @@
 package web
 
 import (
+	"github.com/go-sql-driver/mysql"
 	"github.com/jinzhu/gorm"
+	"github.com/mattn/go-sqlite3" // sqlite3
 	he "httpentity"
+	"httpentity/util"
 	"periwinkle/cfg"
 )
 
@@ -16,10 +19,29 @@ func (p database) Before(req *he.Request) {
 }
 
 func (p database) After(req he.Request, res *he.Response) {
+	defer func() {
+		if obj := recover(); obj != nil {
+			switch err := obj.(type) {
+			case sqlite3.Error:
+				if err.Code == sqlite3.ErrConstraint {
+					*res = he.StatusConflict(heutil.NetString(err.Error()))
+					return
+				}
+			case mysql.MySQLError:
+				// TODO: detect constraint falure for MySQL
+			}
+			// we didn't intercept the error, so pass it along
+			panic(obj)
+		}
+	}()
+
+	if obj := recover(); obj != nil {
+		panic(obj)
+	}
+
 	transaction := req.Things["db"].(*gorm.DB)
-	result := transaction.Commit()
-	if result.Error != nil {
-		// TODO: DB: handle the error; it could be either HTTP 500
-		// (Internal Server Error) or 409 (Conflict)
+	err := transaction.Commit().Error
+	if err != nil {
+		panic(err)
 	}
 }
