28261b9da553c74170b85cc6f65261ba67215f72
Richard L. Wisniewski <r.wisniewski1101@gmail.com>
2015-10-29 19:05:49 -0400

    www - fixed login dialog sources

--- www/src/user/UserController.js
+++ www/src/user/UserController.js
@@ -24,6 +24,38 @@
 			}
 		};
 
+		self.info = {
+			status: {
+				loading: false,
+				error:	''
+			},
+			title:		'USER.INFO.TITLE',
+			addresses:	[],
+			fullName:	'',
+			set_fullName:	function() {
+				//open dialogue
+			},
+			edit_address:	function(index) {
+				
+			},
+			save_edit_address:	function(index) {
+				
+			}
+		};
+		self.groups = {
+			status:		{
+				loading:	false,
+				error:		''
+			},
+			list:		[],
+			'new':		function() {
+				
+			},
+			new_data:	{
+				//new group data
+			}
+		};
+		
 		self.username = 'Richard Wisniewski';
 		self.email = 'rwisniew@purdue.edu';
 		self.sessionID = "0x1234567890";
