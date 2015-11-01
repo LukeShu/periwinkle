47954655819ba9ff84fe38024ea5b13177540e48
Luke Shumaker <lukeshu@sbcglobal.net>
2015-11-01 14:04:26 -0500

    listen-http: fix unix sockets

--- src/periwinkle/executables/listen-http/main.go
+++ src/periwinkle/executables/listen-http/main.go
@@ -10,6 +10,7 @@ import (
 	"os/signal"
 	sd "parabola.nshd/src/sd_daemon"
 	"periwinkle/listeners/web"
+	"strconv"
 	"strings"
 	"syscall"
 )
@@ -19,13 +20,26 @@ func usage(w io.Writer) {
 		`Usage: %s [ADDR_TYPE] [ADDR]
 Do the HTTP that you do, baby.
 
-Address types are "tcp", "tcp4", "tcp6", "unix", and "systemd".  If
-only one argument is given, if it matches one of type it is taken to
-be the type; otherwise it is taken as an address.  The default address
-for "tcp", "tcp4", and "tcp6" is ":8080"; the default "unix" address
-is "/dev/stdin"; "systemd" doesn't have an address.  If the address is
-given, the type is assumed to be "unix" if it contains a slash, or
-"tcp" otherwise.  If no arguments are given, "tcp" is used.
+Address types are "tcp", "tcp4", "tcp6", "unix", and "fd".
+
+If only one argument is given, if it matches one of type it is taken
+to be the type; otherwise it is taken as an address.
+
+  | type            | default address |
+  |-----------------+-----------------|
+  | tcp, tcp4, tcp6 | :8080           |
+  | unix            | ./http.sock     |
+  | fd              | stdin           |
+
+If only the address is given, the type is assumed to be "unix" if it
+contains a slash, "fd" if it only contains numeric digits or matches
+one of the special "fd" values (below), or "tcp" otherwise.  If no
+arguments are given, "tcp" is used.
+
+The address for "fd" is numeric; however, there are several special
+cases. "stdin", "stdout", and "stderr" are aliases for "0", "1", and
+2", respectively. "systemd" causes it to look up the file descriptor
+from systemd socket-activation.
 
 If one argument is given, and it starts with a "-" or is "help", then
 this message is displayed.
@@ -50,17 +64,22 @@ func parse_args() net.Listener {
 			saddr = ":8080"
 		case "unix":
 			stype = os.Args[1]
-			saddr = "/dev/stdin"
-		case "systemd":
+			saddr = "./http.sock"
+		case "fd":
 			stype = os.Args[1]
+			saddr = "stdin"
+		case "systemd", "stdin", "stdout", "stderr":
+			stype = "fd"
+			saddr = os.Args[1]
 		case "help":
 			usage(os.Stdout)
 			os.Exit(0)
 		default:
-			switch {
-			case strings.ContainsRune(os.Args[1], '/'):
+			if strings.ContainsRune(os.Args[1], '/') {
 				stype = "unix"
-			default:
+			} else if _, err := strconv.Atoi(os.Args[1]); err == nil {
+				stype = "fd"
+			} else {
 				stype = "tcp"
 			}
 			saddr = os.Args[1]
@@ -76,8 +95,23 @@ func parse_args() net.Listener {
 	var socket net.Listener
 	var err error
 
-	if saddr == "systemd" {
-		socket, err = sd_get_socket()
+	if stype == "fd" {
+		switch saddr {
+		case "systemd":
+			socket, err = sd_get_socket()
+		case "stdin":
+			socket, err = listenfd(0, "/dev/stdin")
+		case "stdout":
+			socket, err = listenfd(1, "/dev/stdout")
+		case "stderr":
+			socket, err = listenfd(2, "/dev/stderr")
+		default:
+			var n int
+			n, err = strconv.Atoi(saddr)
+			if err == nil {
+				socket, err = listenfd(n, "/dev/fd/"+saddr)
+			}
+		}
 	} else {
 		socket, err = net.Listen(stype, saddr)
 		if tcpsock, ok := socket.(*net.TCPListener); ok {
@@ -91,9 +125,11 @@ func parse_args() net.Listener {
 	return socket
 }
 
+func listenfd(fd int, name string) (net.Listener, error) {
+	return net.FileListener(os.NewFile(uintptr(fd), name))
+}
+
 func sd_get_socket() (socket net.Listener, err error) {
-	socket = nil
-	err = nil
 	fds := sd.ListenFds(true)
 	if fds == nil {
 		err = fmt.Errorf("Failed to aquire sockets from systemd")
