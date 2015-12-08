3cad9929e0c5727d6cb2c76ff1b6c296e83d4c1c
Davis Webb <davislwebb@ymail.com>
2015-12-07 19:51:27 -0500

    Added Test Cases
    Added more test cases in for user.go
    
    There is an issue with GetUserByID() where it is unable to find a record of
    the test user. I could not find the issue before this commit, but am going
    to continue to look.

--- src/periwinkle/backend/user_test.go
+++ src/periwinkle/backend/user_test.go
@@ -1,4 +1,5 @@
 // Copyright 2015 Davis Webb
+
 package backend
 
 import (
@@ -9,7 +10,91 @@ import (
 	"github.com/jinzhu/gorm"
 )
 
-func CreateTempCfg() *periwinkle.Cfg {
+func TestNewUser(t *testing.T) {
+
+	cfg := CreateTempDB()
+
+	user := NewUser(cfg.DB, "JohnDoe", "password", "johndoe@purdue.edu")
+
+	switch {
+	case strings.Compare(user.ID, "JohnDoe") != 0:
+		t.Error("User ID was not properly set to.")
+	case strings.Compare(user.FullName, "") != 0:
+		t.Error("User name was not properly set.")
+	case strings.Compare(user.Addresses[0].Address, "johndoe@purdue.edu") != 0:
+		t.Error("User address was not preperly set.")
+	}
+}
+
+func TestGetUserByID(t *testing.T) {
+	cfg := CreateTempDB()
+
+	user := NewUser(cfg.DB, "JohnDoe", "password", "johndoe@purdue.edu")
+
+	o := GetUserByID(cfg.DB, user.ID)
+
+	switch {
+	case o == nil:
+		t.Error("GetUserByID() returned nil")
+	case strings.Compare(user.ID, o.ID) != 0:
+		t.Error("Error in GetUserByID()")
+	}
+}
+
+func TestNewUserAddress(t *testing.T) {
+
+	cfg := CreateTempDB()
+
+	user := NewUser(cfg.DB, "JohnDoe", "password", "johndoe@purdue.edu")
+
+	newAddr := NewUserAddress(cfg.DB, user.ID, "email", "johndoe2@purdue.edu", false)
+
+	switch {
+	case strings.Compare(newAddr.Address, "johndoe2@purdue.edu") != 0:
+		t.Error("Error adding new email to user in NewUserAddress()")
+	case strings.Compare(newAddr.Medium, "email") != 0:
+		t.Error("Error assigning medium type in NewUserAddress()")
+	}
+
+	newAddr = NewUserAddress(cfg.DB, user.ID, "sms", "7655555555", false)
+
+	switch {
+	case strings.Compare(newAddr.Address, "7655555555") != 0:
+		t.Error("Error adding new sms to user in NewUserAddress()")
+	case strings.Compare(newAddr.Medium, "sms") != 0:
+		t.Error("Error assigning medium type in NewUserAddress()")
+	}
+
+}
+
+func TestGetUserByAddress(t *testing.T) {
+	cfg := CreateTempDB()
+
+	user := NewUser(cfg.DB, "JohnDoe", "password", "johndoe@purdue.edu")
+
+	o := GetUserByAddress(cfg.DB, "email", user.Addresses[0].Address)
+	if strings.Compare(user.ID, o.ID) != 0 {
+		t.Error("Error in GetUserByAdress()")
+	}
+}
+
+func TestSetPassword(t *testing.T) {
+	t.Error("TODO")
+}
+
+func TestCheckPassword(t *testing.T) {
+	t.Error("TODO")
+}
+
+func TestGetAddressByIDAndMedium(t *testing.T) {
+	t.Error("TODO")
+}
+
+func TestGetUserSubscriptions(t *testing.T) {
+	t.Error("TODO")
+}
+
+func CreateTempDB() *periwinkle.Cfg {
 	cfg := periwinkle.Cfg{
 		Mailstore:      "./Maildir",
 		WebUIDir:       "./www",
@@ -31,21 +116,4 @@ func CreateTempCfg() *periwinkle.Cfg {
 	DbSchema(cfg.DB)
 
 	return &cfg
-}
-
-func TestNewUser(t *testing.T) {
-	t.Log("Testing NewUser")
-
-	var cfg *periwinkle.Cfg
-
-	cfg = CreateTempCfg()
-
-	user := NewUser(cfg.DB, "JohnDoe", "password", "johndoe@purdue.edu")
-
-	switch {
-	case strings.Compare(user.ID, "JohnDoe") != 0:
-		t.Error("User ID was not properly set to the user object")
-	case strings.Compare(user.FullName, "") != 0:
-		t.Error("User name was not properly set to the user object")
-	}
 }
